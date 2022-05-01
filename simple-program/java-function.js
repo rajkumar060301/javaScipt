d7375cf3bbc74ba45ac1c1a7c6839646e9df3981
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2022-05-01 23:25:31 +0530

    create javascript progran

---
+++ simple-program/java-function.js
@@ -0,0 +1,4 @@
+var a = 67;
+var b = 65;
+var c = a + b;
+console.log("Your ans is " + c);
--- simple-program/javaScript-prigran.js
+++ simple-program/javaScript-prigran.js
@@ -8,4 +8,32 @@ function sayHello() {
     alert('hee boss');
 }
 
+document.getElementById(div);
 document.write("hii gagan");
+
+function problem() {
+    document.write("Entering the loop!<br /> ");
+    outerloop: // This is the label name
+        for (var i = 0; i < 5; i++) {
+            document.write("Outerloop: " + i + "<br />");
+            innerloop:
+                for (var j = 0; j < 5; j++)
+                    Javascript
+            61
+            {
+                if (j > 3) break; // Quit the innermost loop
+                // if (i == 2) break innerloop; // Do the same thing
+                if (i == 4) break outerloop; // Quit the outer loop
+                document.write("Innerloop: " + j + " <br />");
+            }
+        }
+    document.write("Exiting the loop!<br /> ");
+
+}
+
+// function over() {
+//     document.write ("Mouse Over");
+// }
+// function out() {
+//     document.write ("Mouse Out");
+// }
